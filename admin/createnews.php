44e27abb85cad1723a9ddba728ce630cb63e4191
yves21 <yves21.haut@gmail.com>
2015-08-24 23:03:46 +0200

    submit du form createnews

--- admin/createnews.php
+++ admin/createnews.php
@@ -1,4 +1,41 @@
-<?php define('BASE','../'); ?>
+<?php define('BASE','../');
+
+require (BASE.'conf/utils.php');
+
+if (isset($_POST['bt_submit'])) {
+    try {
+        $bdd = new PDO ('mysql:host=localhost;dbname=bpt', 'root', '');
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(Exception $e) {
+        die('Erreur : '.$e->getMessage());
+    }
+
+    $prefixImageName = generateRandomString();
+
+    // Where the file is going to be placed
+    $target_path = "../upload/";
+
+    $imageName = $prefixImageName.basename($_FILES['image']['name']);
+
+    $target_path = $target_path.$imageName;
+
+    if(move_uploaded_file($_FILES['image']['tmp_name'], $target_path)) {
+        echo "The file ".  basename( $_FILES['image']['name']).
+        " has been uploaded";
+    } else{
+        echo "There was an error uploading the file, please try again!";
+    }
+
+    $stmt = $bdd->prepare('INSERT INTO news(subject, summary, content, image, created) VALUES (:subject, :summary, :content, :image, :created)');
+    $stmt->bindParam(':subject', $_POST['subject']);
+    $stmt->bindParam(':summary', $_POST['summary']);
+    $stmt->bindParam(':content', htmlspecialchars($_POST['content']));
+    $stmt->bindParam(':image',  $imageName);
+    $stmt->bindParam(':created', $_POST['created']);
+    $stmt->execute();
+}
+?>
+
 <!doctype html>
 <html>
 	<!--[if lt IE 9]>
@@ -11,7 +48,8 @@
 		</title>
 
 		<?php include ("../parts/meta-css.php"); ?>
-
+        <link rel="stylesheet" href="<?= BASE ?>css/admin.css" >
+        <script src="<?= BASE ?>lib/ckeditor/ckeditor.js"></script>
 	</head>
 	<body>
 		<div id="container" class="container-fluid">
@@ -20,23 +58,54 @@
 
             <div>
 
-               <form action="createnews.php" method="post" name="createnews" id="createnews" enctype="multipart/form-data">
+               <form action="createnews.php" method="post" class="form-horizontal" name="createnews" id="createnews" enctype="multipart/form-data">
                     <fieldset id="fs_general">
                         <legend>Créer une news</legend>
-                        <label for="subject">Sujet : </label>
-                        <input type="text" name="subject" id="subject" maxlength="120" />
-                        <label for="summary">Résumé : </label>
-                        <input type="text" name="summary" id="summary" maxlength="255" />
-                        <label for="content">Contenu :</label>
-                        <textarea name="content" id="content"></textarea>
-                        <label for="image">Image : </label>
-                        <input type="file" name="image" id="image" accept="image/*" />
-                        <label for="created">Date : </label>
-                        <input type="datetime" name="created" id="created" />
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label" for="subject">Sujet : </label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" name="subject" id="subject" maxlength="120" placeholder="Sujet de la news" />
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label" for="summary">Résumé : </label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" name="summary" id="summary" maxlength="255" placeholder="Résumé de la news" />
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label" for="content">Contenu :</label>
+                            <div class="col-sm-10">
+                                <textarea name="content" id="content" placeholder="Tapez ici votre contenu"></textarea>
+                                <script>
+                                    // Replace the <textarea id="editor1"> with a CKEditor
+                                    // instance, using default configuration.
+                                    CKEDITOR.replace( 'content' );
+                                </script>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label" for="image">Image : </label>
+                            <div class="col-sm-10">
+                                <input type="file" class="form-control" name="image" id="image" accept="image/*" />
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label" for="created">Date : </label>
+                            <div class="col-sm-10">
+                                <input type="date" class="form-control" name="created" id="created" placeholder="24-08-2015" />
+                            </div>
+                        </div>
                     </fieldset>
                     <fieldset id="fs_buttons">
-                        <input type="reset" id="bt_reset" name="bt_reset" value="Reset" />
-                        <input type="submit" id="bt_submit" name="bt_submit" value="Send" />
+                        <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label"></label>
+                            <div class="col-sm-10">
+                                <input type="reset" class="btn btn-warning" id="bt_reset" name="bt_reset" value="Reset" />
+                                <input type="submit" class="btn btn-primary" id="bt_submit" name="bt_submit" value="Send" />
+                            </div>
+                        </div>
                     </fieldset>
                 </form>
 
