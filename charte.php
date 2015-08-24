1d00de70a5ed28dce491631f3d2a6257e7856606
Jean-Louis <soldiertt@gmail.com>
2015-08-24 14:40:49 +0200

    Update and rename charte.html to charte.php

---
+++ charte.php
@@ -0,0 +1,86 @@
+<?php define('BASE','./'); ?>
+<!doctype html>
+<html>
+	<!--[if lt IE 9]>
+　　　　<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+    <![endif]-->
+
+    <head>
+		<title>
+			burningphoenixteam.fr
+		</title>
+
+		<?php include (BASE."parts/meta-css.php"); ?>
+
+	</head>
+	<body>
+		<div id="container" class="container-fluid">
+
+            <?php include (BASE."parts/header.php"); ?>
+
+            <div>
+
+               <h1>Charte des Burning Phoenix</h1>
+
+                <p>Communauté de joueurs, la team Burning Phoenix a pour vocation d’offrir à ses membres un espace pour assouvir pleinement leur passion.</p>
+
+                <p>La team accueille des joueurs de tous niveaux autour des valeurs de Fair Play, d’entraide et de solidarité.</p>
+
+                <p>Les joueurs Burning Phoenix s’engagent dans la team en fonction de leur temps et de leurs envies. Le maître mot de l’activité de la team est le jeu pour le fun.</p>
+
+                <p>Les administrateurs et modérateurs de la team ont pour mission de contribuer à son animation et de veiller à son bon fonctionnement.</p>
+
+                <h2>Usage du Forum</h2>
+
+                <p>Les propos racistes, antisémites, négationnistes, sexistes et homophobes sont proscrits et seront effacés sans préavis. Il en sera de même pour tous liens ou messages à caractère pornographique et de manière générale pour toutes thématiques interdites par les lois en vigueur. L'incitation au piratage est également proscrite.</p>
+
+                <p>Les propos tenus sur ce forum n'engagent que leur auteur. Les administrateurs et modérateurs s'efforcent de faire respecter la présente charte. En conséquence, ils ne sauraient être tenus pour responsable de tous propos tenus sur ce forum et ne respectant pas les règles exposées ici.</p>
+
+                <p>Il importe que chaque membre s’efforce d’écrire dans un langage compréhensible sans efforts, en évitant le langage SMS.</p>
+
+                <p>Le Forum est modéré a posteriori.</p>
+
+                <p>Les modérateurs et administrateurs sont garants d’une expression fluide et respectueuse sur le Forum. Toutefois, leur intervention doit être compatible avec une expression libre et responsable des joueurs de la team dans le respect des principes édictés ci dessus. Le forum reste un lieu de détente pour la communauté que nous sommes, l'équipe d'administration et de modération veille à ce que les nécessaires régulations qu'implique le fonctionnement d'une communauté ne deviennent pas des contraintes fortes pour les joueurs qui la constituent.</p>
+
+                <p>Il est rappelé que l'écrit complique parfois la compréhension du message adressé : blague, provocation mineure, jeu, insulte... il est parfois délicat de faire la différence! Si un usager du forum se sent blessé par des propos tenus sur celui ci, il est souhaitable qu'une discussion s'engage par MP avec l'auteur du message incriminé et /ou un modérateur afin de déterminer le sens exact des propos tenus.</p>
+
+                <p>Enfin, un certain nombre de sujets (politique, religion, sport...) ne sont pas l'objet principal de ce forum. Il est donc recommandé de ne pas en abuser et de ne pas oublier que mal manipulés, ces sujets ont tendance à se comporter de manière explosive et peuvent même entraîner l'action subite de modérateurs en cas de dérapage.</p>
+
+                <h2>Gestion des sections</h2>
+
+                <p>Lorsqu'une section de jeu comporte un effectif suffisant, elle peut bénéficier d'un ou deux coordinateurs de section, avec rôle de modérateur sur les parties du forum du jeu ainsi que sur le teamspeak. Les coordinateurs sont désignés sur proposition des membres de la section concernée et après accord de l'équipe d'administration.</p>
+
+                <h2>Comportement en jeu</h2>
+
+                <p>La team accueillant des joueurs de tous niveaux, il est de la responsabilité de chacun de veiller à ce que les nouveaux joueurs en particulier soient accompagnés dans leur progression dans le jeu. De la même manière, une vigilance particulière est apportée au fait que tous les joueurs puissent trouver leur place dans les différentes activités du clan.</p>
+
+                <p>La participation aux tournois et divers ladder est laissée à l’appréciation individuelle de chacun. Aussi, nous n'imposons aucunement une participation systématique des joueurs du clan aux ladders des différents jeux représentés de la clan. De même, pour ceux qui y participent, nous souhaitons tout mettre en œuvre pour que cela n'induise pas de situations de rivalité à l'intérieur de clan. Il est donc fondamental que chacun veille à ce que les rencontres entre BPT en particulier en ladder ne puissent se produire qu'entre joueurs qui le souhaite vraiment et de manière totalement régulière. Le plus important reste là aussi la logique d'entraide en particulier par le partage de replay, par l'analyse de partie, par des trainings en 1VS1 custom.</p>
+
+                <p>Les comportements agressifs vis à vis des joueurs ou clans rencontrés, l'utilisation de bug exploit et autres pratiques border line sont proscrites.</p>
+
+                <h2>Admission de nouveaux joueurs</h2>
+
+                <p>La décision d'ouverture d'une phase de recrutement sur un jeu, de pauses dans les recrutements d'une section en particulier pour favoriser une bonne intégration des nouveaux et enfin de promotion des activités du clan dans des espaces extérieurs est de la responsabilité de l'équipe d'administration</p>
+
+                <p>Tout joueur qui le souhaite peut postuler pour une entrée dans la team en prenant contact par la rédaction d'un post de présentation dans le topic de recrutement. Une période de test sera réalisée par tout nouvel arrivant, qui appartiendra pendant cette phase au groupe postulant BPT. Cette période de test a avant tout pour but de s'assurer d'une bonne possibilité d'intégration des nouveaux joueurs au sein de la team et d'une réelle adhésion de ceux ci à l'esprit qui anime l'ensemble des joueurs Burning Phoenix. Elle durera environ un mois, à condition que le joueur soit suffisamment présent et actif sur le TS du clan et qu'il soit doté d'un micro. La décision d'évolution d'un joueur est prise par les administrateurs après consultation des joueurs concernés, et le cas échéant des coordinateurs de section.</p>
+
+                <p>A l'issue de la période de test, les nouveaux joueurs deviennent padawan et reçoivent l'accès aux parties stratégiques du forum. Au bout de 5 mois environ les joueurs passeront membre BPT sur décision de l'équipe d'administration après consultation des membres et coordinateurs.</p>
+
+                <p>La team n'accepte que les candidatures de joueurs ayant atteint l'âge de 18 ans. Aucune exception ne sera faite. Les éventuelles candidatures de joueurs d'âge inférieur seront de fait automatiquement rejetées.</p>
+
+                <h2>Bannissement</h2>
+
+                <p>La décision de bannissement définitif d'un usager du forum non membre de la team pour non respect de la présente charte ne peut être prise qu'après accord de 2 membres du staf.<br/>
+                La décision de bannissement temporaire ou définitif d'un membre de la team ne peut être prise qu'après décision par un vote aux 2/3 des administrateurs et modérateurs. Les bannissements temporaires d'une durée inférieure ou égale à 48 heures peuvent être décidés par un seul administrateur.</p>
+
+
+                <p>NB : Pour les joueurs postulant pour Star Citizen, il est interdit d'avoir un double compte ou être affilié sur 2 clans.</p>
+
+
+            </div>
+			 <?php include (BASE."parts/footer.php"); ?>
+		</div>
+        <?php include (BASE."parts/js-script.php"); ?>
+	</body>
+
+</html>
