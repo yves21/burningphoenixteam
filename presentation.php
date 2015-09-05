a0fd4fa9a1dd911065354dd1f3f42417411a4712
yves21 <yves21.haut@gmail.com>
2015-09-05 16:33:14 +0200

    update de la page presentation

--- presentation.php
+++ presentation.php
@@ -19,7 +19,14 @@ require(BASE."conf/auth-config.php");
             <div>
 
                <h1>Présentation</h1>
-               <h2>Test auto deploy - test 2</h2>
+                <h2><p>Clan Francophone qui existe depuis 2007, crée autour du jeu Supreme Commander.</p>
+                    <p>En 2008, il renait pour devenir la Burning Phoenix Team. Depuis le clan embrasa en marquant de son empreinte, l'univers du monde du jeu de la stratégie. </p>
+                    <p>Le clan a pour vocation d’offrir à ses membres un espace pour assouvir pleinement leur passion. </p>
+                    <p>Il accueille des joueurs de tous niveaux autour des valeurs de Fair Play, d’entraide et de solidarité. </p>
+                    <p>Nous regroupons des joueurs de 18 à plus de 50 ans, nous n’avons pas de règles de présence, nous ne sommes pas des adeptes d'une hiérarchie pure et dure. Seul le respect et le partage sont de mises!</p>
+                    <p>Au fil du temps, le clan est devenu multigaming et son forum, une banque de données importantes non seulement dans le monde du jeu vidéo mais aussi sur des sujets de notre vie quotidienne.</p>
+                    <p>Vous recherchez un clan mature, à l'écoute, sans contrainte de présence, vous voulez embraser le monde du jeu vidéo, alors rejoignez les Burning Phoenix Team !</p>
+</h2>
 
             </div>
 			 <?php include (BASE."parts/footer.php"); ?>
