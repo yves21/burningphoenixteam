c97370a93b6ec69eaa6efb96196ca2fe7397d887
yves21 <yves21.haut@gmail.com>
2015-09-23 21:08:18 +0200

    page recrutement

--- recrut.php
+++ recrut.php
@@ -18,9 +18,32 @@ require(BASE."conf/auth-config.php");
 
             <div class="text-block">
 
-               <h1>Recrutement</h1>
+                <h1>Recrutement</h1>
 
-               <h2>Test</h2>
+                <h2>Développement des sections suivantes (il n'y a pas que Star Citizen), contactez-nous sur le forum et sur le TS pour plus d'informations :</h2>
+
+
+                <ul>
+                    <li><strong>Arma :</strong> <a href='http://burningphoenixteam.vraiforum.com/f169-ARMA-III-DayZ.htm'>http://burningphoenixteam.vraiforum.com/f169-ARMA-III-DayZ.htm</a>   (Serveur BPT)</li>
+
+                    <li><strong>Act Of Agression :</strong> <a href='http://burningphoenixteam.vraiforum.com/f190-Act-of-Aggression.htm'>http://burningphoenixteam.vraiforum.com/f190-Act-of-Aggression.htm</a></li>
+
+                    <li><strong>Project Zombie :</strong> <a href='http://burningphoenixteam.vraiforum.com/f229-Project-Zomboid.htm'>http://burningphoenixteam.vraiforum.com/f229-Project-Zomboid.htm</a>  (Serveur BPT), jeu multi très peu gourmand en ressource,</li>
+
+                    <li><strong>Compagny Of Heroes II :</strong> <a href='http://burningphoenixteam.vraiforum.com/f140-Compagny-of-Heroes-2.htm'>http://burningphoenixteam.vraiforum.com/f140-Compagny-of-Heroes-2.htm</a></li>
+
+                    <li><strong>Wargame Red Dragon :</strong> <a href='http://burningphoenixteam.vraiforum.com/f171-Wargame-Red-Dragon.htm'>http://burningphoenixteam.vraiforum.com/f171-Wargame-Red-Dragon.htm</a></li>
+
+                    <li><strong>Ark Survival :</strong> <a href='http://burningphoenixteam.vraiforum.com/f226-ARK-Survival-Evolved.htm'>http://burningphoenixteam.vraiforum.com/f226-ARK-Survival-Evolved.htm</a> (Serveur BPT)</li>
+
+                    <li><strong>Supreme Commander :</strong> <a href='http://burningphoenixteam.vraiforum.com/f5-test-1.htm'>http://burningphoenixteam.vraiforum.com/f5-test-1.htm</a></li>
+
+                    <li><strong>Space Origin :</strong> <a href='http://burningphoenixteam.vraiforum.com/f250-Space-Origin.htm'>http://burningphoenixteam.vraiforum.com/f250-Space-Origin.htm</a></li>
+
+                    <li><strong>World Of the Tank :</strong> <a href='http://burningphoenixteam.vraiforum.com/f120-World-of-tanks.htm'>http://burningphoenixteam.vraiforum.com/f120-World-of-tanks.htm</a></li>
+
+                    <li><strong>War Thunder :</strong> Escadrille BPT relancée : <a href='http://burningphoenixteam.vraiforum.com/f143-War-Thunder.htm'>http://burningphoenixteam.vraiforum.com/f143-War-Thunder.htm</a></li>
+                </ul>
             </div>
 			 <?php include (BASE."parts/footer.php"); ?>
 		</div>
