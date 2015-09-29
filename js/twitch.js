9c215cf2b6d62c69690efeb87753095386b988b5
yves21 <yves21.haut@gmail.com>
2015-09-29 21:55:35 +0200

    timer twitch

--- js/twitch.js
+++ js/twitch.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
-    var channels = ['yves21haut','guig_esprit_du_sage','thomasfreddy56','drangonlee'];
 
-    channels.forEach(function(channel) {
-        $.getJSON("https://api.twitch.tv/kraken/streams/"+channel+"?callback=?",function(streamData) {
-            console.log('Stream Data:', streamData) // debug message, shows all available data
-            if(streamData && streamData.stream) {
-                // displayed if online
-                $('#twitch' + channel).html("<img src='image/green_ball_16.png' alt='ONLINE' />");
-            } else {
-                // displayed if offline
-                $('#twitch' + channel).html("<img src='image/red_ball_16.png' alt='OFFLINE' />");
-            }
+    var refreshTwitchStatus = function() {
+        var channels = ['yves21haut','guig_esprit_du_sage','thomasfreddy56','drangonlee'];
+        channels.forEach(function(channel) {
+            $.getJSON("https://api.twitch.tv/kraken/streams/"+channel+"?callback=?",function(streamData) {
+                console.log('Stream Data:', streamData) // debug message, shows all available data
+                if(streamData && streamData.stream) {
+                    // displayed if online
+                    $('#twitch' + channel).html("<img src='image/green_ball_16.png' alt='ONLINE' />");
+                } else {
+                    // displayed if offline
+                    $('#twitch' + channel).html("<img src='image/red_ball_16.png' alt='OFFLINE' />");
+                }
+            });
         });
-    });
+    };
+    refreshTwitchStatus();
+    setInterval(refreshTwitchStatus, 120000);
 });
