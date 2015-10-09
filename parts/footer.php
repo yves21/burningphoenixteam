69f31b6c2258f3f965c2d7a86c5df6028c26b487
soldiertt <soldiertt@gmail.com>
2015-10-09 21:33:25 +0200

    add footer links

--- parts/footer.php
+++ parts/footer.php
@@ -1,8 +1,18 @@
 <footer>
 
-<div>
-    <div>
+<div id="container" class="container-fluid">
+
+    <div class="row">
+        <div class="col-md-3" style="margin-top:20px">
+            <a href="http://www.starcitizen.fr/" target="_blank"><img src="image/starcitizenfr.png" width="300" /></a><br/>
+            <a href="http://www.jeux-strategie.com/" target="_blank"><img src="image/logojs.png" height="100" /></a>
+        </div>
+        <div class="col-md-6">
         <img src="<?= BASE ?>image/logo.png" alt="Logo">
+        </div>
+        <div class="col-md-3" style="margin-top:20px">
+            <a href="https://robertsspaceindustries.com/" target="_blank"><img src="image/rsilogo.png" /><img src="image/rsilogo2.png" /></a>
+        </div>
     </div>
 </div>
 
