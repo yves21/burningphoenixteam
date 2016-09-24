6bf9adee17e86316d062cc1cf80c501184c1bea5
soldiertt <soldiertt@gmail.com>
2016-09-24 15:46:34 +0200

    fix rest calendar url

--- json/cal-events.php
+++ json/cal-events.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-$oneYearAgoTime = strtotime("-1 year", time());
+$oneYearAgoTime = strtotime("-6 month", time());
 $oneYearAgoDate = date("c", $oneYearAgoTime);
 $url = "https://www.googleapis.com/calendar/v3/calendars/bpt.burningphoenixteam@gmail.com/events?showDeleted=false&updatedMin=".urlencode($oneYearAgoDate)."&key=AIzaSyBDIor8rccGDkDD4oHmBFlmZnVv-rO3Y-k";
 $json = file_get_contents($url, 0, null, null);
